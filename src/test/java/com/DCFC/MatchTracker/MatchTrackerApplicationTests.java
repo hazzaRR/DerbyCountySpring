7c718e18bd648764a2133f9a392fd6695e2f8c91
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-08-28 19:58:14 +0100

    initial setup of application

---
+++ src/test/java/com/DCFC/MatchTracker/MatchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DCFC.MatchTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
