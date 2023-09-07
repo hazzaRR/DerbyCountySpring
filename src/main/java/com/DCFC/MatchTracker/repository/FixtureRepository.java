9381c57b821a3f14417c6a5b5fa18e8f9569776e
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-09-07 14:45:10 +0100

    fixed bug with or statments for upcoming fixtures SELECT statements

--- src/main/java/com/DCFC/MatchTracker/repository/FixtureRepository.java
+++ src/main/java/com/DCFC/MatchTracker/repository/FixtureRepository.java
@@ -12,7 +12,7 @@ public interface FixtureRepository extends JpaRepository<Fixture, Integer> {
     @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1%", nativeQuery = true)
     List<Fixture> findFixturesByYear(String year);
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND competition = ?2 AND stadium = ?3 AND home_team = ?4 or away_team = ?4",
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND competition = ?2 AND stadium = ?3 AND (home_team = ?4 OR away_team = ?4)",
             nativeQuery = true)
     List<Fixture> findFixturesByYearCompetitionStadiumTeam(String year, String competition, String stadium, String team);
 
@@ -25,16 +25,16 @@ public interface FixtureRepository extends JpaRepository<Fixture, Integer> {
     @Query(value = "SELECT * FROM upcoming_fixtures WHERE competition = ?1", nativeQuery = true)
     List<Fixture> findFixturesByCompetition(String competition);
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE stadium = ?1 AND away_team = ?2 OR home_team = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE stadium = ?1 AND (away_team = ?2 OR home_team = ?2)", nativeQuery = true)
     List<Fixture> findFixturesByStadiumTeam(String stadium, String team);
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE competition = ?1 AND away_team = ?2 OR home_team = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE competition = ?1 AND (away_team = ?2 OR home_team = ?2)", nativeQuery = true)
     List<Fixture> findFixturesByCompetitionTeam(String competition, String team);
 
     @Query(value = "SELECT * FROM upcoming_fixtures WHERE competition = ?1 AND stadium = ?2", nativeQuery = true)
     List<Fixture> findFixturesByCompetitionStadium(String competition, String stadium);
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND away_team = ?2 OR home_team = ?2", nativeQuery = true)
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND (away_team = ?2 OR home_team = ?2)", nativeQuery = true)
     List<Fixture> findFixturesByYearTeam(String year, String team);
 
     @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND stadium = ?2", nativeQuery = true)
@@ -43,16 +43,16 @@ public interface FixtureRepository extends JpaRepository<Fixture, Integer> {
     @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND competition = ?2", nativeQuery = true)
     List<Fixture> findFixturesByYearCompetition(String year, String competition);
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE competition = ?1 AND stadium = ?2 AND away_team = ?3 OR home_team = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE competition = ?1 AND stadium = ?2 AND (away_team = ?3 OR home_team = ?3)", nativeQuery = true)
     List<Fixture> findFixturesByCompetitionStadiumTeam(String competition, String stadium, String team);
 
     @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND competition = ?2 AND stadium = ?3", nativeQuery = true)
     List<Fixture> findFixturesByYearCompetitionStadium(String year, String competition, String stadium);
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND competition = ?2 AND away_team = ?3 OR home_team = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND competition = ?2 AND (away_team = ?3 OR home_team = ?3)", nativeQuery = true)
     List<Fixture> findFixturesByYearCompetitionTeam(String year, String competition, String team);
 
 
-    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND stadium = ?2 AND away_team = ?3 OR home_team = ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM upcoming_fixtures WHERE kickoff LIKE %?1% AND stadium = ?2 AND (away_team = ?3 OR home_team = ?3)", nativeQuery = true)
     List<Fixture> findFixturesByYearStadiumTeam(String year, String stadium, String team);
 }
