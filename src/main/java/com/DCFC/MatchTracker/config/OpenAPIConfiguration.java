41cd7902bd6e7f8f9dc3cd52a1474a5391f9ff7a
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-02-06 20:48:50 +0000

    added configuration file for openapi docs

---
+++ src/main/java/com/DCFC/MatchTracker/config/OpenAPIConfiguration.java
@@ -0,0 +1,37 @@
+package com.DCFC.MatchTracker.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfiguration {
+
+    @Bean
+    public OpenAPI defineOpenApi() {
+        Server server1 = new Server();
+        server1.setUrl("http://localhost:8080");
+        server1.setDescription("Development");
+
+        Server server2 = new Server();
+        server2.setUrl("http://derbycounty.harryredman.com");
+        server2.setDescription("Production");
+
+        Contact myContact = new Contact();
+        myContact.setName("Harry Redman");
+        myContact.setEmail("harryredman.dev@gmail.com");
+
+        Info information = new Info()
+                .title("Derby County API")
+                .version("1.0")
+                .description("This API exposes endpoints to retrieve all past and present Derby County match data and upcoming fixtures")
+                .contact(myContact);
+        return new OpenAPI().info(information).servers(List.of(server1));
+    }
+}
